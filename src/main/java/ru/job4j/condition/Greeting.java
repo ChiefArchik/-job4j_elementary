d339cdb735fa9e9577f0399d0d11ce7713d636c0
ChiefArchik <git@github.com:ChiefArchik/job4j_elementary.git>
2021-09-27 00:11:56 +0300

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java! ";
+        String newbie = "But I am a newbie. ";
+        int age = 2021;
+        String id = idea + newbie + age;
+        System.out.println(id);
+        }
+}
