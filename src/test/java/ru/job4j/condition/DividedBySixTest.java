6b097a79d5ca11f89f0620c0f47d82934388594b
ChiefArchik <git@github.com:ChiefArchik/job4j_elementary.git>
2021-10-01 22:01:38 +0300

    4.4. Вложенный оператор if else [#190]

---
+++ src/test/java/ru/job4j/condition/DividedBySixTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class DividedBySixTest {
+
+    @Test
+    public void whenNuberDividedBy6() {
+        int in = 24;
+        String result = DividedBySix.checkNumber(in);
+        String expected = "Исходное число делится на 6.";
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenNumberDividedBy3AndNotEven() {
+        int in = 9;
+        String result = DividedBySix.checkNumber(in);
+        String expected = "Исходное число делится на 3, но не является четным.";
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenNumberNoDividedBy3AndEven() {
+        int in = 14;
+        String result = DividedBySix.checkNumber(in);
+        String expected = "Число не делится на 3, но является четным.";
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenNumberNoDividedBy3AndNotEvrn() {
+        int in = 25;
+        String result = DividedBySix.checkNumber(in);
+        String expected = "Исходное число не делится на 3 и не является четным.";
+        Assert.assertEquals(expected, result);
+    }
+}
