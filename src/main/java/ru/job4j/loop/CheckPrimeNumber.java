70e4a6e0359310755ffc55a187ee96c0fbb7c951
ChiefArchik <git@github.com:ChiefArchik/job4j_elementary.git>
2021-10-11 23:35:28 +0300

    5.6. Простые числа [#156314]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -5,9 +5,6 @@ public class CheckPrimeNumber {
     public static boolean check(int number) {
         boolean prime = true;
         int hold;
-        if (number == 1) {
-            prime = false;
-        }
         for (int index = 2; index <= number / 2; index++) {
             hold = number % index;
             if (hold == 0) {
