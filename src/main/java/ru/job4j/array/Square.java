a9e93282b4a3275a06e963dd759f86986e09189f
ChiefArchik <git@github.com:ChiefArchik/job4j_elementary.git>
2021-10-14 15:42:44 +0300

    6.0.5. Массивы в тестах. [#226789]

--- src/main/java/ru/job4j/array/Square.java
+++ src/main/java/ru/job4j/array/Square.java
@@ -11,8 +11,8 @@ public class Square {
 
     public static void main(String[] args) {
         int[] array = calculate(4);
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+        for (int j : array) {
+            System.out.println(j);
         }
     }
 }
