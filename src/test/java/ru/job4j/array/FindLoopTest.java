d97d2dd825d64264f9f4e0f1cb2a5f5b3168197e
ChiefArchik <git@github.com:ChiefArchik/job4j_elementary.git>
2021-10-14 16:00:31 +0300

    6.1. Классический поиск перебором. [#33489]

---
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FindLoopTest {
+
+    @Test
+    public void whenArrayHas5Than0() {
+        int[] data = {5, 2, 3, 4};
+        int el = 5;
+        int result = FindLoop.index0f(data, el);
+        int expected = 0;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenArrayHas9Than2() {
+        int[] data = {6, 2, 3, 4};
+        int el = 9;
+        int result = FindLoop.index0f(data, el);
+        int expected = -1;
+        Assert.assertEquals(expected, result);
+    }
+}
