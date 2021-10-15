e88946a7c7834e808e6c49353e4cef8063310ee8
ChiefArchik <git@github.com:ChiefArchik/job4j_elementary.git>
2021-10-15 17:48:22 +0300

    6.2.3. Перевернуть массив. [#226790]

--- src/test/java/ru/job4j/array/TurnTest.java
+++ src/test/java/ru/job4j/array/TurnTest.java
@@ -23,7 +23,7 @@ public class TurnTest {
 
     @Test
     public void whenTurnArrayWithEvenAmountOfElementsThenTurnedArray2() {
-        int[] input = new int[]{4, 1, 6, 2, 7, 10, 44, 55,};
+        int[] input = new int[]{4, 1, 6, 2, 7, 10, 44, 55};
         int[] result = Turn.back(input);
         int[] expected = new int[]{55, 44, 10, 7, 2, 6, 1, 4};
         Assert.assertArrayEquals(expected, result);
