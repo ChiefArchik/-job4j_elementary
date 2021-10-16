ad85c288e83ec8d4c62e8c6b2fb6e8049261b5c8
ChiefArchik <git@github.com:ChiefArchik/job4j_elementary.git>
2021-10-16 11:39:54 +0300

    6.4.2. Слово заканчивается на ... [#156317]

---
+++ src/test/java/ru/job4j/array/EndWithTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class EndWithTest {
+
+    @Test
+    public void whenEndWithPostfixThenTrue() {
+        char[] word = new char[] {'H', 'e', 'l', 'l', 'o'};
+        char[] post = new char[] {'l', 'o'};
+        boolean result = EndWith.endWith(word, post);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void whenEndWithPostfixThenFalse() {
+        char[] word = new char[] {'H', 'e', 'l', 'l', 'o'};
+        char[] post = new char[] {'l', 'a'};
+        boolean result = EndWith.endWith(word, post);
+        Assert.assertFalse(result);
+    }
+}
