c2c395f686d4d3c514ec8a5eb48efef0cd37ff15
ChiefArchik <git@github.com:ChiefArchik/job4j_elementary.git>
2021-10-17 09:23:41 +0300

    6.3. Массив заполнен true или false [#53857 #216490]

--- src/main/java/ru/job4j/array/Check.java
+++ src/main/java/ru/job4j/array/Check.java
@@ -3,11 +3,11 @@ package ru.job4j.array;
 public class Check {
     public static boolean mono(boolean[] data) {
         boolean result = true;
-        for (int i = 0; i < data.length; i++) {
-            if (data[0] != data[i]) {
+        for (boolean datum : data) {
+            if (data[0] != datum) {
                 return false;
             }
         }
-        return result;
+        return true;
     }
 }
