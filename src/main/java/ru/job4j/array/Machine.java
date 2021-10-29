5909234dc41e0a8c9e47e1532827cf111dc740db
ChiefArchik <git@github.com:ChiefArchik/job4j_elementary.git>
2021-10-29 17:53:55 +0300

    6.9. Жадный алгоритм. Сдача в кофе машине [#34741]

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -8,10 +8,10 @@ public class Machine {
         int[] rsl = new int[100];
         int size = 0;
         int remain = money - price;
-        for (int i = 0; i < coins.length; i++) {
-            while (remain >= coins[i]) {
-                remain = remain - coins[i];
-                rsl[size] = coins[i];
+        for (int coin : coins) {
+            while (remain >= coin) {
+                remain = remain - coin;
+                rsl[size] = coin;
                 size++;
             }
         }
