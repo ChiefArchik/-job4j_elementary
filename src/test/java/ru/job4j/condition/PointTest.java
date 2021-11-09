d29bbc41f92d48b2e47d7cd7bc4318ada1c95111
ChiefArchik <git@github.com:ChiefArchik/job4j_elementary.git>
2021-11-09 22:43:33 +0300

    2.1. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -7,35 +7,26 @@ import org.junit.Assert;
 public class PointTest {
     @Test
     public void when01to23then2dot82() {
-        double expected = 2.82;
-        int x1 = 0;
-        int y1 = 1;
-        int x2 = 2;
-        int y2 = 3;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Point a = new Point(0, 1);
+        Point b = new Point(2, 3);
+        double dist = a.distance(b);
+        System.out.println(dist);
     }
 
     @Test
     public void when89to13then9dot21() {
-        double expected = 9.21;
-        int x1 = 8;
-        int y1 = 9;
-        int x2 = 1;
-        int y2 = 3;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Point a = new Point(8, 9);
+        Point b = new Point(1, 3);
+        double dist = a.distance(b);
+        System.out.println(dist);
     }
 
     @Test
     public void when27to23then4() {
-        double expected = 4;
-        int x1 = 2;
-        int y1 = 7;
-        int x2 = 2;
-        int y2 = 3;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Point a = new Point(2, 7);
+        Point b = new Point(2, 3);
+        double dist = a.distance(b);
+        System.out.println(dist);
     }
 
 }
